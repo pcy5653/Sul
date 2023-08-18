b9e9741014a16efcf396a8561bde19eee04da7e8
kimseeun10 <tpdms981@gmail.com>
2023-08-18 14:25:42 +0900

    BOARD

---
+++ src/main/java/com/alcohol/sul/board/notice/NoticeController.java
@@ -0,0 +1,24 @@
+package com.alcohol.sul.board.notice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping("/board/*")
+public class NoticeController {
+	
+	@Autowired
+	private NoticeService noticeService;
+	
+	// List 
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/alcohol/sul/board/notice/NoticeService.java
@@ -0,0 +1,15 @@
+package com.alcohol.sul.board.notice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+	
+	//List
+	
+
+}
