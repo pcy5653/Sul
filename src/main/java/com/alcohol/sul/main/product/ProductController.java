ddf9efaba722082973f9e85c757bbeb8243d4448
kmj7777777 <dngu_icdi@naver.com>
2023-09-04 16:17:07 +0900

    c

--- src/main/java/com/alcohol/sul/main/product/ProductController.java
+++ src/main/java/com/alcohol/sul/main/product/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	//전체 List
+	// 전체 List
 	@GetMapping(value = "list")
 	public String getList(PagerK pager, Model model) throws Exception {
 		List<ProductDTO> ar = productService.getList(pager);
@@ -39,7 +39,7 @@ public class ProductController {
 		List<ProductDTO> ar = productService.getListTakju(pager);
 		model.addAttribute("list", ar);
 		model.addAttribute("pager", pager);		
-		return "product/takjuList";
+		return "product/list";
 	}
 	
 	//약청주 List
@@ -48,7 +48,7 @@ public class ProductController {
 		List<ProductDTO> ar = productService.getListChungju(pager);
 		model.addAttribute("list", ar);
 		model.addAttribute("pager", pager);		
-		return "product/chungjuList";
+		return "product/list";
 	}
 	
 	//과실주 List
@@ -57,7 +57,7 @@ public class ProductController {
 		List<ProductDTO> ar = productService.getListWine(pager);
 		model.addAttribute("list", ar);
 		model.addAttribute("pager", pager);		
-		return "product/wineList";
+		return "product/list";
 	}
 	
 	//증류주 List
@@ -66,7 +66,7 @@ public class ProductController {
 		List<ProductDTO> ar = productService.getListSoju(pager);
 		model.addAttribute("list", ar);
 		model.addAttribute("pager", pager);		
-		return "product/sojuList";
+		return "product/list";
 	}
 
 	
